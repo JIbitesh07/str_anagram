e2b1f44602c37761d351622be9c44b0fe333038c
JIbitesh07 <66659260+JIbitesh07@users.noreply.github.com>
2020-07-03 14:40:14 +0530

    created

---
+++ check_2str_anagram.py
@@ -0,0 +1,8 @@
+s1=input("Enter a string: ")
+s2=input("Enter another string: ")
+sorted_1=sorted(s1)
+sorted_2=sorted(s2)
+if(sorted_1==sorted_2):
+    print("Its a anagram")
+else:
+    print("Its not a anagram")
